6de818033ddf191fcdac0462acf7aef29cf4c7ed
DanielDiBartoloit <danieldibartoloit@gmail.com>
2022-08-02 23:30:27 -0300

    Create project

---
+++ src/test/java/com/alkemy/movies/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.movies.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
