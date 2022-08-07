52a83cae5110742b4e98b8d6a448b20b64391329
DanielDiBartoloit <danieldibartoloit@gmail.com>
2022-08-07 17:10:21 -0300

    Add movie mapper and repository

--- src/main/java/com/alkemy/movies/api/controller/MovieController.java
+++ src/main/java/com/alkemy/movies/api/controller/MovieController.java
@@ -2,15 +2,15 @@ package com.alkemy.movies.api.controller;
 
 import com.alkemy.movies.api.dto.MovieDTO;
 import com.alkemy.movies.api.service.MovieService;
+import com.alkemy.movies.api.service.impl.MovieServiceImpl;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -24,11 +24,15 @@ public class MovieController {
 
     @PostMapping
     public ResponseEntity<MovieDTO> saveMovie(@RequestBody MovieDTO movie) {
-
         MovieDTO savedMovie = movieService.save(movie);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
     }
+    @GetMapping
+    public ResponseEntity<List<MovieDTO>> getAllMovies() {
+        List<MovieDTO> movies = movieService.getAllMovies();
+        return ResponseEntity.ok().body(movies);
+    }
+
 
 
 }
---
+++ src/main/java/com/alkemy/movies/api/service/impl/MovieServiceImpl.java
@@ -0,0 +1,37 @@
+package com.alkemy.movies.api.service.impl;
+
+import com.alkemy.movies.api.dto.MovieDTO;
+import com.alkemy.movies.api.entity.MovieEntity;
+import com.alkemy.movies.api.mapper.MovieMapper;
+import com.alkemy.movies.api.repository.MovieRepository;
+import com.alkemy.movies.api.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+    @Autowired
+    private MovieMapper movieMapper;
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public MovieDTO save(MovieDTO dto) {
+        MovieEntity entity = movieMapper.movieDTO2Entity(dto);
+        MovieEntity savedEntity = movieRepository.save(entity);
+        MovieDTO result = movieMapper.movieEntity2DTO(savedEntity);
+        return result;
+    }
+
+    public List<MovieDTO> getAllMovies(){
+        List<MovieEntity> entities = movieRepository.findAll();
+        List<MovieDTO> result = movieMapper.moviesEntityList2DTOList(entities);
+        return result;
+    }
+
+}
+
+
+
